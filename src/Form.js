b4dfbad3e3daf07a070f58bac18002e7657c67a4
kunalkhadke <kunal.khadke@hybrowlabs.com>
2020-08-01 16:32:24 +0530

    push

--- src/Form.js
+++ src/Form.js
@@ -1,77 +1,71 @@
-import React from "react";
-import { Formik, Form, Field, connect } from 'formik';
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 class LoginForm extends React.Component {
-
-    ValidateUser(value) {
-        let error;
-        if (value === "Kunal") {
-            error = "good try"
-        }
-        else {
-            return error
-        }
-    }
-
-    ValidatePassword(value) {
-        let error;
-        if (!value) {
-            error = "please fill the password"
-        }
-        else {
-            return error
-        }
-    }
     render() {
         return (
-            <div>
-                <div class="container"
-                    style={{
-                        textAlign: "center",
-                        border: '1px outset blue',
-                        backgroundColor: 'lightblue',
-                        margin: 0,
-                        padding: 10,
-                        // position: "absolute",
-                        top: "20%",
-                        left: "20%",
-                    }} >
-                    <Formik
-                        initialValues={{
-                            username: '',
-                            email: '',
-                            password: '',
-                            phonenumber: '',
-                            address: ''
-                        }}
-                        onSubmit={values => {
-                            // same shape as initial values
-                            this.props.history.push({ pathname: '/userdetail', state: values })
-                            console.log(values);
-                        }}
-                    >
-                        {({ errors, touched, isValidating }) => (
-                            <Form>
-                                <Field name="username" validate={this.ValidateUser()} placeholder='username' />
-                                <br></br>
-                                {errors.username && touched.username && <div>{errors.username}</div>}
-                                <Field name="email" name='email' placeholder='email' />
-                                <br></br>
-                                <Field name="password" validate={this.ValidatePassword()} placeholder='password' />
-                                {errors.password && touched.password && <div>{errors.password}</div>}
-                                <br></br>
-                                <Field name="phonenumber" placeholder='phonenumber' />
-                                <br></br>
-                                <Field name="address" placeholder='address' />
-                                <br></br>
-                                <button type="submit">LOGIN</button>
-                            </Form>
-                        )
-                        }
-                    </Formik>
-                </div>
-            </div>
+            <Formik
+                initialValues={{
+                    username: '',
+                    email: '',
+                    password: '',
+                    phonenumber: '',
+                    address: ''
+                }}
+                validationSchema={Yup.object().shape({
+                    username: Yup.string()
+                        .required('User Name is required'),
+                    email: Yup.string()
+                        .email('Email is invalid')
+                        .required('Email is required'),
+                    password: Yup.string()
+                        .min(6, 'Password must be at least 6 characters')
+                        .required('Password is required'),
+                    phonenumber: Yup.string()
+                        .min(10, 'phone number must be at least 10 characters')
+                        .required('phone number is required'),
+                })}
+                onSubmit={values => {
+                    alert('SUCCESS!! :-)\n\n' + JSON.stringify(values, null, 4))
+                    this.props.history.push({ pathname: '/userdetail', state: values })
+                }}
+                render={({ errors, status, touched }) => (
+                    <Form>
+                        <div className="form-group">
+                            <label htmlFor="username">User Name</label>
+                            <Field name="username" type="text" className={'form-control' + (errors.username && touched.username ? ' is-invalid' : '')} />
+                            <ErrorMessage name="username" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <Field name="email" type="text" className={'form-control' + (errors.email && touched.email ? ' is-invalid' : '')} />
+                            <ErrorMessage name="email" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
+                            <ErrorMessage name="password" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="phonenumber">Phone number</label>
+                            <Field name="phonenumber" type="password" className={'form-control' + (errors.phonenumber && touched.phonenumber ? ' is-invalid' : '')} />
+                            <ErrorMessage name="phonenumber" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="address">address</label>
+                            <Field name="address" type="password" className={'form-control' + (errors.address && touched.address ? ' is-invalid' : '')} />
+                            <ErrorMessage name="address" component="div" className="invalid-feedback" />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary mr-2">Register</button>
+                            <button type="reset" className="btn btn-secondary">Reset</button>
+                        </div>
+                    </Form>
+                )}
+            />
         )
     }
 }
+
 export default LoginForm;
